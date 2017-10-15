c40caedc09e9974b086181b9570f457d64edc8bb
Ketill <ketill95@gmail.com>
2017-10-15 20:20:54 +0000

    útfærði test fyrir '', '1', '1,2'

--- src/test/java/stringCalKata/stringCalcTest.java
+++ src/test/java/stringCalKata/stringCalcTest.java
@@ -8,6 +8,8 @@ public class stringCalcTest{
     @Test
     public void testEmptyString(){
         assertEquals(0, stringCalc.add(""));
+        assertEquals(1, stringCalc.add("1"));
+        assertEquals(3, stringCalc.add("1,2"));
     }
 
     public static void main(String args[]){
